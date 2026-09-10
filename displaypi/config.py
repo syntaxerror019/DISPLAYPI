f7596976d39afabe7b9d23c17b6eb3929eb5eada
Miles Hilliard <skywiredvt@gmail.com>
2026-09-09 22:38:07 -0400

    updated default scroll speed

--- displaypi/config.py
+++ displaypi/config.py
@@ -10,7 +10,7 @@ COUNTDOWNS_FILE = DATA_DIR / "countdowns.json"
 CASCADED_MATRICES = 20
 BLOCK_ORIENTATION = -90  # Default for most 4-in-1 modules. Change to 90 or 0 if scrolling is wrong.
 DISPLAY_ROTATE = 2  # 0=0°, 1=90°, 2=180°, 3=270°
-SCROLL_DELAY = 0.015
+SCROLL_DELAY = 0.012
 BRIGHTNESS = 128  # 0 to 255
 
 # --- Weather Settings ---
